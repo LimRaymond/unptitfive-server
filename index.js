4cc9d51f780e4eddab9909197d562be5e0e7a439
Romain <romainlaemmel@gmail.com>
2020-09-30 13:19:29 +0200

    ESLint configuration

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
-let port = process.env.PORT || 3000;
+const express = require('express');
 
-app.get("/", (req, res) => {
-    res.send("Hello world 2");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world 2');
 });
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
-})
+});
