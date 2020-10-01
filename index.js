b8e8b4a7f707ffd6ccc7ddb30e829a99ef7f9ed9
Romain <romainlaemmel@gmail.com>
2020-10-01 16:41:42 +0200

    Test Docker + Mongo

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User.model');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+const connection = 'mongodb://mongo:27017/unptitfive';
+
+mongoose.connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello world 2');
+    res.send('Hello world');
+});
+
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+app.get('/user-create', async (req, res) => {
+    const user = new User({ username: 'John' });
+    await user.save().then(() => {
+        console.log('User created');
+    });
+    res.send('User created');
 });
 
 app.listen(port, () => {
