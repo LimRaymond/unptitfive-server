c4ae1737396416850ba03289e69fc911dfd8a091
Romain <romainlaemmel@gmail.com>
2020-10-06 18:37:50 +0200

    Add socket connection

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const utils = require('./utils/utils');
 
-const app = express();
 const port = process.env.PORT || 3000;
 const db = process.env.DATABASE_URL || 'mongodb://localhost:27017/unptitfive';
 
@@ -22,6 +24,20 @@ routes.routes.forEach((r) => {
     app.use(r.name, r.router);
 });
 
-app.listen(port, () => {
+io.use(async (socket, next) => {
+    if (socket.handshake.query && socket.handshake.query.token) {
+        const user = await utils.getUserByToken(socket.handshake.query.token);
+        if (!user) return next(new Error('Authentication error'));
+        socket.user_id = user._id;
+        return next();
+    }
+    return next(new Error('Authentication error'));
+});
+
+io.sockets.on('connection', (socket) => {
+    console.log(`New connection (${socket.user_id})`);
+});
+
+server.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
--- utils/utils.js
+++ utils/utils.js
@@ -5,6 +5,7 @@ const config = require('../config.json');
 function getUserByToken(token) {
     return new Promise((resolve, reject) => {
         jwt.verify(token, config.JWT_SECRET, (err, decode) => {
+            if (err) reject(err);
             User.findOne({ _id: decode, token }, (err, user) => {
                 if (err) reject(err);
                 resolve(user);
