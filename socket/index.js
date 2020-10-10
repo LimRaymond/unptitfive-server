522a1dbdbe01c17482c53925686c85844c60be74
Romain <romainlaemmel@gmail.com>
2020-10-10 09:21:49 +0200

    Link socket to database

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,6 @@
 const ent = require('ent');
+const Channel = require('../models/channel.model');
+const Message = require('../models/message.model');
 const utils = require('../utils/utils');
 
 function startSocket(io) {
@@ -16,35 +18,69 @@ function startSocket(io) {
     io.sockets.on('connection', (socket) => {
         console.log(`New connection (${socket.user._id})`);
 
-        socket.on('join', (channel) => {
-            if (socket.channel) {
-                socket.leave(socket.channel, () => {
-                    io.in(socket.channel).emit('data', {
-                        message: `<b style="color: teal;">${socket.user.username} has left the channel</b>`,
+        socket.on('join', async (channelName) => {
+            const channel = await Channel.findOne({ name: channelName });
+            if (channel) {
+                // Leave the old channel
+                if (socket.channel) {
+                    socket.leave(socket.channel.name, () => {
+                        io.in(socket.channel.name).emit('info', {
+                            message: `${socket.user.username} has left the channel`,
+                            color: 'teal',
+                        });
+                    });
+                }
+
+                // Send old channel messages to the newcomer
+                const messages = await Message.find({ channel: channel._id }).populate('user', 'username');
+                const oldMessages = [];
+                messages.forEach((m) => {
+                    oldMessages.push({
+                        username: m.user.username,
+                        message: m.message,
+                        date: utils.convertDate(m.date),
+                    });
+                });
+                socket.emit('message', oldMessages);
+
+                // Join the new channel
+                socket.channel = channel;
+                socket.join(channel.name, () => {
+                    io.in(socket.channel.name).emit('info', {
+                        message: `${socket.user.username} has joined the channel`,
+                        color: 'teal',
                     });
                 });
             }
-            socket.channel = channel;
-            socket.join(channel, () => {
-                io.in(channel).emit('data', {
-                    message: `<b style="color: teal;">${socket.user.username} has joined the channel</b>`,
-                });
-            });
         });
 
-        socket.on('message', (message) => {
+        socket.on('message', async (message) => {
             if (socket.channel) {
+                // Encode message
                 message = ent.encode(message);
-                io.in(socket.channel).emit('data', {
-                    message: `<b>${socket.user.username}</b> : ${message}`,
+
+                // Save message in database
+                const newmessage = new Message({
+                    message,
+                    user: socket.user._id,
+                    channel: socket.channel._id,
                 });
+                const res = await newmessage.save();
+
+                // Broadcast message
+                io.in(socket.channel.name).emit('message', [{
+                    username: socket.user.username,
+                    message,
+                    date: utils.convertDate(res.date),
+                }]);
             }
         });
 
         socket.on('disconnect', () => {
             if (socket.channel) {
-                io.in(socket.channel).emit('data', {
-                    message: `<b style="color: teal;">${socket.user.username} has left the channel</b>`,
+                io.in(socket.channel.name).emit('info', {
+                    message: `${socket.user.username} has left the channel`,
+                    color: 'teal',
                 });
             }
         });
