ab63d32520920be06106d7da6b5d1c0265e765a3
Romain <romainlaemmel@gmail.com>
2020-10-29 19:28:21 +0100

    Get messages by channel name rather than channel id

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,19 +1,19 @@
 const Message = require('../models/message.model');
+const Channel = require('../models/channel.model');
 const { translate } = require('../utils/utils');
 
 async function getMessages(req, res) {
-  if (req.params.channel_id) {
-    try {
-      const messages = await Message.find({ channel: req.params.channel_id }).populate('user', 'username');
+  const lang = req.acceptsLanguages();
+  if (req.params.channel_name) {
+    const channel = await Channel.findOne({ name: req.params.channel_name });
+    if (channel) {
+      const messages = await Message.find({ channel: channel.id }).populate('user', 'username');
       return res.status(200).json(messages);
-    } catch (error) {
-      const lang = req.acceptsLanguages();
-      return res.status(400).json({ message: translate('ERROR_STRING_INVALID', lang, 'channel_id') });
     }
-  } else {
-    const messages = await Message.find();
-    return res.status(200).json(messages);
+    return res.status(400).json({ message: translate('ERROR_STRING_INVALID', lang, 'channel_name') });
   }
+  const messages = await Message.find();
+  return res.status(200).json(messages);
 }
 
 exports.getMessages = getMessages;
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/message.controller');
 const auth = require('../middlewares/auth');
 
-router.get('/:channel_id?', auth, controller.getMessages);
+router.get('/:channel_name?', auth, controller.getMessages);
 
 module.exports = router;
